94ae5c9a77158881c20d2d7fdfdb1d9d967046be
unknown <johnferreira141@gmail.com>
2022-04-04 21:06:15 -0300

    Primeiro commit react-notes

---
+++ src/components/AddNote.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+
+const AddNote = ({handleAddNote}) => {
+    const [noteText, setNoteText] = useState('');
+    const characterLimit = 200;
+
+    const handleChange = (event) => {
+        if (characterLimit - event.target.value.length >= 0) {
+            setNoteText(event.target.value)
+        };
+    };
+
+    const handleSaveClick = () => {
+        if (noteText.trim().length > 0) {
+            handleAddNote(noteText);
+            setNoteText('');
+        }
+       // handleAddNote(noteText);
+    }
+
+    return (
+        <div className="note new">
+            <textarea
+                rows="8"
+                cols="10"
+                placeholder="Digite aqui"
+                value={noteText}
+                onChange={handleChange}>
+                
+            </textarea>
+        
+       
+        <div className="note-footer">
+                <small>{characterLimit - noteText.length} Caracteres restantes </small>
+            <button className="save" onClick={handleSaveClick}>Salvar</button>
+         </div>
+        </div>
+    )
+    
+}
+
+export default AddNote;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Post-it Virtual com React</h1>
+            
+        </div>
+
+    )
+}
+//<button className="save">Tema</button>
+export default Header;
